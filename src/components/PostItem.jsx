bfde1178fe4fa8e84ee4037d74eff9d7a2a0d984
janemir <janemiro2005@gmail.com>
2024-11-28 21:23:53 +0300

    props

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,20 @@
+import PostItem, { useState } from "react";
+
+let PostItem = (props) => {
+
+    return (
+        <div className="post">
+            <div className="post__contend">
+                <strong>{props.post.id}{props.post.title}</strong>
+                <div>
+                    {props.post.body}
+                </div>
+            </div>
+            <div className="post__btns">
+                <button>удалить</button>
+            </div>
+        </div>
+    )
+}
+
+export default PostItem;
