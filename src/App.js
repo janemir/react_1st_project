ea67bea91c341058bafa1b803ad014b2ebaac6af
janemir <janemiro2005@gmail.com>
2024-11-28 22:11:42 +0300

    delete import PostItem

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
 
 function App() {
-let [posts, setPosts] = useState([
-  {id: 1, title: 'Javascript', body: 'Description'}
-  {id: 2, title: 'Javascript 2', body: 'Description'}
-  {id: 3, title: 'Javascript 3', body: 'Description'}
-])
+  let [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ])
 
   return (
     <div className="App">
-      {posts.map(post =>
-        <PostItem post={post}/>
+      <h1 style={{ textAlign: 'center' }}>Список постов</h1>
+      {posts.map((post) =>
+        <PostItem post={post} key={post.id} />
       )}
     </div>
   );
